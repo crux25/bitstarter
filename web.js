a0c492d6f776e2a9cab14298731f87c517f37715
crux25 <amos@Genie.(none)>
2013-07-12 19:55:06 +0100

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var data = fs.readFileSync('index.html');
+var out = data.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
